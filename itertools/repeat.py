71345499e827f38fe72d37d3d759505b3533a424
chrisjdavie <cjdavie@googlemail.com>
2017-05-11 19:15:38 +0000

    repeat is interesting

--- itertools/repeat.py
+++ itertools/repeat.py
@@ -1,5 +1,11 @@
 
-from itertools import repeat
+from itertools import repeat, islice
+
+for elem in islice(repeat('A'), 10):
+    print(elem)
 
 
+print()
 
+for elem in repeat('A', 10):
+    print(elem)
