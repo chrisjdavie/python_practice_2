bb6e0a1d00ac7ebce17752bade82d17fe59de8ed
chrisjdavie <cjdavie@googlemail.com>
2017-05-11 19:33:16 +0000

    compress is a pythonic way of masking

---
+++ itertools/compress.py
@@ -0,0 +1,11 @@
+
+from itertools import compress, cycle
+
+for el in compress([ 1, 2, 3, 4, 5 ], [ True, False, True, False, True ]):
+    print(el)
+
+print()
+
+for el in compress([ 1, 2, 3, 4, 5 ], cycle([1, 0])):
+    print(el)
+
